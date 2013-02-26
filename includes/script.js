4408b99ae381e2d3dd85a5307653138ae8ddd439
Wain <wain-pc@mail.ru>
2013-02-26 23:22:50 +0400

    Initial release
    First version with some minor fixes to popups event handling. Currently
    works only on ebay.com, international eBay sites have absolutely
    different structure, therefore half of the code should be re-written.

---
+++ includes/script.js
@@ -0,0 +1,273 @@
+﻿ // ==UserScript==
+// @include http://ebay.com/itm/*
+// @include https://ebay.com/itm/*
+// @include http://*.ebay.com/itm/*
+// @include https://*.ebay.com/itm/*
+// @include       http://www.ebay.co.uk/itm/*
+// @include       http://www.ebay.nl/itm/*
+// @include       http://www.ebay.ch/itm/*
+// @include       http://www.ebay.at/itm/*
+// @include       http://www.ebay.de/itm/*
+// @include       http://www.ebay.in/itm/*
+// @include       http://www.ebay.ie/itm/*
+// @include       http://www.ebay.nl/itm/*
+// @include       http://www.ebay.ph/itm/*
+// @include       http://www.ebay.pl/itm/*
+// @include       http://www.ebay.com.hk/itm/*
+// @include       http://www.benl.ebay.be/itm/*
+// @include       http://cgi.ebay.*
+// @include       http://www.befr.ebay.be/itm/*
+// @include       http://www.ebay.it/itm/*
+// @include       http://www.ebay.com.my/itm/*
+// @include       http://www.ebay.com.sg/itm/*
+// @include       http://www.ebay.es/itm/*
+// @include       http://www.ebay.com.au/itm/*
+// @include       http://www.ebay.ca/itm/*
+// @include	      http://www.ebay.fr/itm/*
+// ==/UserScript==
+
+var LOGIN;
+
+ function sendLoginMessage()
+    {
+	var login = window.$("#enterGixenLogin").val();
+	var password = window.$("#enterGixenPassword").val();
+	if(login && password) {
+	var data={'message':'login','login':login,'password':password};
+	opera.extension.postMessage(data);	// send message from injected script to background process
+	openGixenPopup('login_loading');
+	return true;
+	}
+	else { alert("Please enter your login and password"); return false;}
+    }
+	
+function sendAddSnipeMessage(isEdited) {
+var maxBid;
+if(!isEdited) { maxBid = window.$("input[name=\"maxbid\"]").val(); }
+else { maxBid = window.$("#editGixenSnipeValue").val(); }
+
+var itemId = window.$(".iti-eu-txt.iti-eu-pd.u-flR.iti-ncntr :first-child").html();
+if(!itemId) itemId = window.$('#itm_num').html();
+if(itemId) {
+			if(isNumber(maxBid)) {
+					var data;
+					if(!isEdited) {
+						data={'message':'addsnipe','itemid':itemId,'maxbid':maxBid};
+						}
+					else {
+						data={'message':'editsnipe','itemid':itemId,'maxbid':maxBid};
+					}
+					opera.extension.postMessage(data);
+					return true;
+			}
+			else { alert("Please enter your bid"); return false; }
+		}
+		else { alert ("Apologies, can not identify item id. Please write a bug report about that."); return false; }
+    }
+	
+	
+function sendActionMessage() {
+var maxBid = window.$("input[name=\"maxbid\"]").val();
+
+if (isNumber(maxBid)) {
+
+	var data={'message':'action'};
+	opera.extension.postMessage(data);
+	openGixenPopup('add_loading'); 
+	return true;
+}
+else { alert("Please input valid snipe value!"); return false; }
+}
+
+
+function receiveMessage(event)
+    {
+    var message=event.data.message;
+		if(message=='got_login') { 
+			LOGIN = event.data.login;
+		}
+	
+		else if(message=='login_success') {
+			if(sendAddSnipeMessage(false)) {
+				openGixenPopup('add_loading',function() {
+					showGixenPopupMessage('Login success, placing your snipe now...'); 
+					});
+				}
+			 }
+		else if (message=='login_required') { openGixenPopup('login',
+			function() {
+				addValueToField('enterGixenLogin',LOGIN);
+				}
+			);
+		}//Trying to log in to Gixen 
+		else if (message=='login_failed') {
+			openGixenPopup('login',function() {
+				if(LOGIN) {
+					addValueToField('enterGixenLogin',LOGIN);
+					window.$("#editGixenSnipeValue").focus();
+				}
+				showGixenPopupMessage('Your eBay login or password is incorrect. Please log in again!');
+				}
+			); 
+		} 
+		
+		else if (message=='snipe_add_success') openGixenPopup('add_success');
+		else if (message=='snipe_already_exists') {
+		openGixenPopup('edit',function() {
+			showGixenPopupMessage(event.data.maxbid);
+			var itemValue = window.$("#MaxBidId").val();
+			addValueToField('editGixenSnipeValue',itemValue);
+			window.$("#editGixenSnipeValue").focus();
+			});
+			}
+		else if (message=='snipe_add_failed') {
+			openGixenPopup('add_success',function() {
+				showGixenPopupMessage("Adding the snipe has failed for some reason. Sorry for that =( ");
+				});
+			}
+    }
+	
+function initialize() {
+	opera.postError('INITIALIZE STARTED');
+	opera.extension.postMessage({'message':'ready'});
+	addPageElements();
+}
+
+function addPageElements() {
+	opera.postError('ADDPAGEELEMENTS STARTED');
+
+	var url = window.location.host;
+	var $bidButton, gixenButtonHTML;
+	if((url=="www.ebay.com") || (url=="cgi.ebay.com")) {
+		$bidButton = window.$("span a#bidBtn_btn");
+		gixenButtonHTML = "<span class=\"s bl vib vibs1\"><a id=\"bidBtn_gixen\" class=\"bt\">									Place Gixen Snipe</a></span>";
+		$bidButton.after("</br></br>"+gixenButtonHTML);	
+		window.$("#bidBtn_gixen").on('click',function(e) {
+			sendActionMessage(); //all the magic starts here!	
+			});
+	}
+	else {
+		opera.postError('NOT EBAY HOST STARTED');
+		loadJQuery(function() {
+			$bidButton = window.$(".bn-b");
+			gixenButtonHTML = "<span class=\"bn-b\"><input id=\"bidBtn_gixen\" value=\"Place Gixen Snipe\" type=\"submit\"></span>";
+		$bidButton.parent().after("</br></br>"+gixenButtonHTML);	
+
+		window.$("#bidBtn_gixen").on('click',function(e) {
+			e.preventDefault();
+			sendActionMessage(); //listener is added here as the request is asyncronous	
+			});		
+			
+		});	
+}
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+
+
+
+
+function loadJQuery(callback) {
+	var fileObj = opera.extension.getFile("/jquery-1.9.1.min.js");
+        
+        if (fileObj) {
+            var fr = new FileReader();
+			fr.onload = function(e) {
+				var contents = document.createTextNode(e.target.result);
+				var body = document.getElementsByTagName('body')[0];
+					if(body) { 
+						var _script = document.createElement('script');
+						_script.appendChild(contents);
+						body.appendChild(_script);
+						opera.postError("JQuery is loaded");
+						if(callback) callback();
+					}
+					else opera.postError("Cannot identify <body> tag!");
+				};
+				
+			fr.readAsText(fileObj);
+		}
+		else alert("Couldn't load JQuery");
+
+}
+
+function openGixenPopup(popup_mode,callback) {
+
+closeGixenPopup(); //Closing previously opened popups, if any exist
+
+var path = '/popups/gixen_'+popup_mode+'_popup.html';
+	
+		var fileObj = opera.extension.getFile(path);
+        
+        if (fileObj) {
+            // Read out the File object as a Data URI:
+            var fr = new FileReader();
+			fr.onload = function(e) {
+				var PopupContents = e.target.result;
+				window.$("body").append(PopupContents);
+				setGixenPopupPosition();
+				addWindowEventListeners();
+				if(callback) callback();
+				};
+			fr.readAsText(fileObj);
+		}
+		else alert("Couldn't load popup login window");
+}
+
+function closeGixenPopup() {
+window.$("#gixenPopupWindow, #gixenWindowBackground").remove();
+}
+
+function setGixenPopupPosition() {
+	var width = window.$(window).width();
+	var height = window.$(window).height();
+	
+	window.$("#gixenPopupWindow").css({'top':(height/2)-175,'left':(width/2)-245});
+}
+
+function showGixenPopupMessage(message) {
+window.$("#messageField").html(message);
+}
+
+function addWindowEventListeners() {
+
+window.$("#popupFormLogin").on('submit',function(e) {
+	e.preventDefault();
+	window.$("#loginToGixen").trigger('click');
+});
+
+window.$("#popupFormEdit").on('submit',function(e) {
+	e.preventDefault();
+	window.$("#editGixenSnipe").trigger('click');
+});
+
+
+window.$("#loginToGixen").on('click',function(e) {
+	sendLoginMessage();
+	});
+
+window.$("#editGixenSnipe").on('click',function(e) {
+	if(sendAddSnipeMessage(true)) {
+		openGixenPopup('add_loading',function() { showGixenPopupMessage('Placing your new snipe now, this may take a few moments...'); });
+	}
+
+});
+
+window.$("#closeGixenPopupWindow, #CancelCloseGixenPopupWindow").on('click',function(e) {
+	closeGixenPopup();
+});
+
+window.$(window).resize(function() {
+	setGixenPopupPosition();
+});
+}
+
+function addValueToField(fieldId,value) {
+window.$('#'+fieldId).val(value);
+}
+
+window.addEventListener('DOMContentLoaded',initialize,false);
+opera.extension.onmessage = receiveMessage;
---
+++ index.js
@@ -0,0 +1,170 @@
+﻿/*
+TODO:
+1. Add button to toolbar with gixen login status and number of snipes pending
+*/
+
+var injected_script_link; // we use this to identify the page we're processing
+var storage = widget.preferences;
+var USERNAME=storage.login;
+
+//assign above function to event handler
+opera.extension.onmessage = receiveMessage;
+
+function receiveMessage(event) { //function receives all the incoming messages and starts corresponding functions
+injected_script_link=event.source;	// save link to injected script that sent this message
+if(event.data.message=='ready') { //Injected script is ready to roll
+	if(USERNAME) {
+		setNewPreferencesLogin(USERNAME); // Send it user login saved in Settings
+	}
+}
+
+else if(event.data.message=='action') { //check if we've already been logged in to Gixen
+
+		sendGetHomePageRequest(function(data) { //Send request to get default homePage
+			if(analyzeServerLoginResponse(data))  { //Page analyzer shows that we're logged in
+				injected_script_link.postMessage({'message':'login_success'});
+			}
+			else injected_script_link.postMessage({'message':'login_required'}); //We're NOT logged in
+		});
+}
+else if(event.data.message=='login') { //need to launch login process
+	sendLoginRequest(event.data.login,event.data.password); //sending login request to server
+	setNewPreferencesLogin(event.data.login); //if the username is different than saved one, replace it 
+	}
+else if(event.data.message=='addsnipe') { addSnipe(event.data.itemid,event.data.maxbid,false); }
+else if(event.data.message=='editsnipe') { addSnipe(event.data.itemid,event.data.maxbid,true); }
+else opera.postError("Gixen extension received unknown message!");
+}
+
+function sendLoginRequest(login,password)
+    {
+	USERNAME = login;
+	var params = "username="+ encodeURIComponent(login) +"&password="+ encodeURIComponent(password) +"&signin=signin&Submit=Log+in+Now";
+	//making request' params string
+	makeAjaxRequest('POST',"http://www.gixen.com/home_1.php",params,function(data) { //sending login AJAX request
+			if(analyzeServerLoginResponse(data)) sendGetHomePageRequest(function() { //if pageAnalyzer shows login success
+							injected_script_link.postMessage({'message':'login_success'}); //then make the injected script to know that
+				});
+			else injected_script_link.postMessage({'message':'login_failed'}); //pageAnalyzer shows that login wasn't successful
+	});
+
+    }
+	
+	
+function sendGetHomePageRequest(callback) //getting default home page HTML from Gixen server, that launch a callback to process it
+    {
+	var params = "gixenlinkcontinue=1&username="+encodeURIComponent(USERNAME);
+	var url = "http://www.gixen.com/home_2.php?username="+USERNAME;
+	makeAjaxRequest('POST',url,params,function(data) {
+		callback(data);
+	});
+    }
+	
+function addSnipe(itemId,maxBid,isModified) { //Adding snipe. If the item already exists, isModified argument must be true
+	var params = "newitemid="+ encodeURIComponent(itemId) +"&newmaxbid="+ encodeURIComponent(maxBid)+"&username="+encodeURIComponent(USERNAME)+"&newsnipegroup=0";
+	if (isModified) params+="&ismodified=1";
+	var url = "http://www.gixen.com/home_2.php?username="+ encodeURIComponent(USERNAME);
+	makeAjaxRequest('POST',url,params,function(data) {
+		var snipingResponse = analyzeServerSnipingResponse(data,itemId); //Using SnipingResponse analyzer to acquire request status
+		if(snipingResponse) injected_script_link.postMessage({'message':snipingResponse}); //send whatever message Analyzer produced back to injected script
+	});
+}
+
+
+function makeAjaxRequest(type,url,params,successCallback) { //making standart XMLHTTPRequest with callback to process returned data
+	xmlHttp = new window.XMLHttpRequest();
+	if(xmlHttp) {
+		if(type=='GET') {
+	xmlHttp.open(type, url, true);
+	}
+	else if (type=='POST') {
+	xmlHttp.open(type, url, true);
+	if(params.length>0) {
+		xmlHttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		}
+	}
+	
+	else { 
+	return -1;
+	}
+	
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4)
+			{
+			successCallback(xmlHttp.responseText);
+			}
+		}
+	xmlHttp.send(params);
+	}
+}
+
+
+function analyzeServerLoginResponse(responseText) { //Login request response analyzer
+	
+	//This function finds certain phrases on the page and makes assumptions about login procedure success
+	//It returns true if login is successful, else false
+	if(responseText.search("If not redirected to your account in 5 seconds, click")!=-1) {
+	return true;
+	}
+	
+	else if(responseText.search("Scheduled bids:")!=-1) {
+	return true;
+	}
+	
+	
+	else if((responseText.search("Password is not a match.")!=-1) || (responseText.search("Wrong password")!=-1)) {
+	return false;
+	}
+	else if(responseText.search("Cookies are disabled in your browser")!=-1) {
+	return false;
+	}
+	
+	else if(responseText.search("Session expired. Please sign in again.")!=-1) { 
+	return false;
+	}
+	return false;
+}
+
+function analyzeServerSnipingResponse(responseText,itemId) {
+//Uses same principal for response analysis, but for making a snipe request
+//Returns status to send back if possible. If not (e.g. asyncronous request needs to be make, returns nothing 
+if(responseText.search("Sorry, this item is already present.")!=-1) {
+	sendGetHomePageRequest(function(data) {
+		var itemProperties = getCurrentItemProperties(data,itemId);
+		injected_script_link.postMessage({'message':'snipe_already_exists','maxbid':itemProperties.maxbid});
+	});
+	return;
+	}
+
+else if(responseText.search("id=\"edititemid\" value=\""+itemId+"\"")!=-1) {
+	return 'snipe_add_success';
+	}
+else {
+	if(analyzeServerLoginResponse(responseText)) return 'snipe_add_failed';
+	else return 'login_required';
+
+}
+	
+}
+
+function getCurrentItemProperties(homePageHTML,itemId) { 
+//This functions analyzes Gixen homePage to get current item's max bid placed
+
+var item = {'maxbid':0};
+var $homePageHTML = $(homePageHTML);
+var $itemHTML = $homePageHTML.find('input[name=edititemid_'+itemId+']').parent();
+
+
+item.id = $itemHTML.find('#edititemid').val();
+item.maxbid = $itemHTML.find('#editmaxbid').val();
+
+return item;
+}
+
+function setNewPreferencesLogin(login) {
+//Placing new login in extension's preferences and sending it back to injected script.
+//Used only on page load and upon every login 
+	storage.login = login;
+	injected_script_link.postMessage({'message':'got_login','login':USERNAME});
+}
+
